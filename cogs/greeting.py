868f5508e20774d969d1377f5481082bbdac1d7f
Edward Paxton <hello@snyco.uk>
2024-01-25 10:51:15 +0800

    Ensuring integer

--- cogs/greeting.py
+++ cogs/greeting.py
@@ -66,7 +66,7 @@ class Greeting(Cog):
                 icon_url = member_after.display_avatar.url
             )
             await member_after.guild.system_channel.send(embed = embed)
-            member_role: Role = member_after.guild.get_role(os.getenv("MEMBER_ROLE"))
+            member_role: Role = member_after.guild.get_role(int(os.getenv("MEMBER_ROLE")))
             await member_after.add_roles(member_role, reason = "Onboard Completed")
 
 async def setup(bot):
