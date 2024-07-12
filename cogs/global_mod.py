db37abaed4502a98001fff132f56c22e210fae86
Snyco <hello@snyco.dev>
2024-07-12 12:04:29 +0800

    Added `WarningPrompt` class for Message Prompts.

--- cogs/global_mod.py
+++ cogs/global_mod.py
@@ -7,6 +7,53 @@ from discord import Button, ButtonStyle, Embed, Interaction, Member, TextChannel
 from discord.ui import View, button
 from discord.app_commands import command, describe
 from discord.ext.commands import Cog
+
+class WarningPrompt(View):
+	def __init__(self, target_user: Member, member_record: dict, reason: str):
+		self.target_user = target_user
+		self.member_record = member_record
+		self.reason = reason
+		super().__init__(timeout = None)
+
+	@button(
+		label = "Ban",
+		style = ButtonStyle.red
+	)
+	async def ban(self, button: Button, interaction: Interaction):
+		try:
+			await self.target_user.ban(
+				reason = self.reason,
+				delete_message_seconds = 60
+			)
+		except:
+			return await interaction.response.send_message(f"{os.getenv("EMOJI_FAIL")} The Royal Defence is not authorised to ban this user.")
+		button.disabled = True
+		button.style = ButtonStyle.green
+		for button_ in self.children:
+			button_.disabled = True
+
+		await interaction.edit_original_response(
+			embed = self.original_embed.add_field(
+				name = "Authorised by:",
+				value = f"<@!{interaction.user.id}>"
+			),
+			view = self
+		)
+
+	@button(
+		label = "Infractions",
+		style = ButtonStyle.blurple
+	)
+	async def infractions(self, button: Button, interaction: Interaction):
+		...
+
+	@button(
+		label = "Release",
+		style = ButtonStyle.grey
+	)
+	async def cancel(self, button: Button, interaction: Interaction):
+		...
+
 class GlobalModeration(Cog):
 	def __init__(self, bot):
 		self.bot = bot
