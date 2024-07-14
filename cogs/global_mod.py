c379463a16bca91e4996ca2eb33b9550d088f27e
Snyco <hello@snyco.dev>
2024-07-14 18:51:59 +0800

    Updated database property names

--- cogs/global_mod.py
+++ cogs/global_mod.py
@@ -71,17 +71,17 @@ class GlobalModeration(Cog):
 			}
 		)
 
-		if guild_config.get("auto_ban"):
-			if member_record.get("global_ban"):
-				await member.ban(reason = member_record.get("global_ban_reason"))
+		if guild_config.get("moderation").get("auto_ban"):
+			if member_record.get("infractions").get("global_ban"):
+				await member.ban(reason = member_record.get("infractions").get("global_ban_reason"))
 
 			else:
 				return
 
 		else:
-			report_channel = self.bot.get_channel(guild_config.get("moderation").get("warning_channel"))
+			report_channel = self.bot.get_channel(guild_config.get("moderation").get("request_channel"))
 			report_embed = Embed(
-				description = f"This member has been banned Federation-wide for:\n```diff\n-{member_record.get('global_ban_reason')}",
+				description = f"This member has been banned Federation-wide for:\n```diff\n-{member_record.get('infractions').get('global_ban_reason')}",
 				colour = 0x2B2D31
 			)
 			return await report_channel.send(
@@ -89,7 +89,7 @@ class GlobalModeration(Cog):
 				view = WarningPrompt(
 					target_user = member,
 					record = member_record,
-					reason = member_record.get("global_ban_reason")
+					reason = member_record.get("infractions").get("global_ban_reason")
 				)
 			)
 
